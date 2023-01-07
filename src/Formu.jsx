51d856ff963fc418eec20ac85d3ab56aca060623
psycobah <@psycobah@hotmail.com>
2023-01-07 02:10:48 -0300

    otras soluciones

--- src/Formu.jsx
+++ src/Formu.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import { Form, FormControl } from "react-bootstrap";
+import React, {useState} from 'react'
+import { Form, FormControl,InputGroup  } from "react-bootstrap";
 
 const Formu = ({setData,data}) => {
 
+
     const nombreRef = React.useRef("")
-  
-    
+    const apellidoRef = React.useRef("")
+    const fechaRef = React.useRef("")
+
+
+    const funcion = (e) =>{
+
+      console.log(e.target.value)
+      
+      console.log(fechaRef.current.value)
+    }
 
   return (
-    <Form>
+    <Form className= "container">
           <h3>Completar Formulario</h3>
-          <FormControl ref={nombreRef} type="text" name="nombre" placeholder="Nombre" onChange={()=>{setData({...data, nombre: nombreRef.current.value})}} ></FormControl>
+          <InputGroup className='gap-5'>
+             <FormControl ref={nombreRef} type="text" name="nombre" placeholder="Nombre" onChange={()=>{setData({...data, nombre: nombreRef.current.value})}} ></FormControl><FormControl ref={apellidoRef} type="text" name="apellido" placeholder="Apellido" onChange={()=>{setData({...data, apellido: apellidoRef.current.value})}} ></FormControl>
+             <FormControl ref={fechaRef} type="date" 
+             onChange={funcion}></FormControl>
+          </InputGroup>
+
+              
+         
+        
+          {/* <Row>
+            
+          <FormControl ref={nombreRef} type="text" name="nombre" placeholder="Nombre" onChange={()=>{setData({...data, nombre: nombreRef.current.value})}} ></FormControl><FormControl ref={apellidoRef} type="text" name="apellido" placeholder="Apellidos" onChange={()=>{setData({...data, apellido: apellidoRef.current.value})}} ></FormControl>
+          </Row> */}
+          
         </Form>
   )
 }
